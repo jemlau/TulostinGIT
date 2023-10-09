9ec26713337657336a33c5d66d2f2e726504723f
jemlau <142881394+jemlau@users.noreply.github.com>
2023-10-09 15:31:42 +0300

    1. committi

--- src/tulostingit.java
+++ src/tulostingit.java
@@ -3,7 +3,23 @@ public class tulostingit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hei olen tulostinohjelma");
+		System.out.print("Ohjelman tekijä ");  
+		String tekijä = "Jemina";
+		System.out.println(tekijä);
+		int luku1 = 20;
+		int luku2 = 5;
+		System.out.println(luku1);
+		System.out.println(luku1 = 60);
+		System.out.println(luku2 = 10);
+		int tulo = 600;
+		System.out.println("60 * 10");
+		int erotus = 50;
+		System.out.println( 60 - 10);
+		int summa = 70;
+		System.out.println( 60 + 10);
+		int osamäärä = 6;
+		System.out.print( 60 / 10);
 	}
 
 }
